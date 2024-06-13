1ae98e86cb171133b32bf746950ec71078ac45ca
Sam Amiri <samamiri@hotmail.com>
2024-06-13 15:34:44 -0400

    dfsdf

--- src/components/ui/Hero.tsx
+++ src/components/ui/Hero.tsx
@@ -19,7 +19,7 @@ const Hero = () => {
         <div className="flex flex-col justify-center space-y-4">
           <div className="space-y-2">
             <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl xl:text-6xl">
-              Elevate Your Ride with Sam's Mobile Detailing
+              {"Elevate Your Ride with Sam's Mobile Detailing"}
             </h1>
             <p className="max-w-[600px] text-gray-500 md:text-xl dark:text-gray-400">
               Experience the ultimate in car care with our comprehensive
