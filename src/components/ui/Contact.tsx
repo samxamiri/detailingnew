8f5b843d69b62f3312053b4a5ba46d42e55dd7ed
Sam Amiri <samamiri@hotmail.com>
2024-06-13 16:11:44 -0400

    fixed errors in contact.tsx

--- src/components/ui/Contact.tsx
+++ src/components/ui/Contact.tsx
@@ -21,11 +21,11 @@ const Contact = () => {
     "The All In One",
   ];
 
-  const handleChange = (e) => {
+  const handleChange = (e: any) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async (e: any) => {
     e.preventDefault();
     try {
       await axios.post("/api/sendEmail", formData);
@@ -169,7 +169,7 @@ const Contact = () => {
                   className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                   id="grid-message"
                   placeholder="Your message..."
-                  rows="3"
+                  rows={3}
                 ></textarea>
               </div>
             </div>
