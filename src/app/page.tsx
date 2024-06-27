9904ac23782338fca74da18888b862dc85375781
Sam Amiri <samamiri@hotmail.com>
2024-06-27 13:14:20 -0400

    added title and meta deescirption to layout.tsx, also removed header and footer sections from page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,11 @@
 import React from "react";
-import Head from "next/head"; // Import the Head component
-// import Packages from "../components/ui/Packages";
-import Header from "../components/ui/Header";
 import Hero from "../components/ui/Hero";
-// import Services from "../components/ui/Services";
-// import BeforeAfter from "../components/ui/BeforeAfter";
 import ServiceArea from "../components/ui/ServiceArea";
 import Benefits from "../components/ui/Benefits";
-// import Workflow from "../components/ui/Workflow";
 import Contact from "../components/ui/Contact";
 import Layout from "../components/ui/Layout";
 import DetailingPrices from "../components/ui/DetailingPrices";
 import Services from "../components/ui/services";
-import Footer from "@/components/ui/Footer";
 
 const imagePairs = [
   {
@@ -44,16 +37,7 @@ const imagePairs = [
 
 const LandingPage = () => {
   return (
-    <>
-      <Head>
-        <title>Sam&apos;s Mobile Detailing</title>
-        <meta
-          name="description"
-          content="Sam's Mobile Detailing offers interior, exterior and complete detailing services in Montreal and surrounding areas. No need to deal with having to drop off your car at a shop, we come to you!"
-        />
-      </Head>
-
-      <Header />
+    <Layout>
       <section id="hero">
         <Hero />
       </section>
@@ -66,22 +50,13 @@ const LandingPage = () => {
       <section id="benefits">
         <Benefits />
       </section>
-      {/* <section id="workflow">
-        <Workflow />
-      </section> */}
-      {/* <section id="gallery">
-        <BeforeAfter imagePairs={imagePairs} />
-      </section> */}
       <section id="serviceArea">
         <ServiceArea />
       </section>
       <section id="contact">
         <Contact />
       </section>
-      <section id="footer">
-        <Footer />
-      </section>
-    </>
+    </Layout>
   );
 };
 
