09f537b8b1456c6205d84df0a40e4b412dca6dd2
Sam Amiri <samamiri@hotmail.com>
2024-06-27 13:22:43 -0400

    added title and meta desc in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,20 @@
-// src/pages/_app.tsx
-
-import type { AppProps } from 'next/app';
-import '/src/app/globals.css'; // Ensure this path is correct
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import "/src/app/globals.css"; // Ensure this path is correct
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Sam&apos;s Mobile Detailing</title>
+        <meta
+          name="description"
+          content="Sam's Mobile Detailing offers interior, exterior and complete detailing services in Montreal and surrounding areas. No need to deal with having to drop off your car at a shop, we come to you!"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
