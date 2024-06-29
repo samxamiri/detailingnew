f6845c48831d39749333d37c2c053bab9197fb6f
Sam Amiri <samamiri@hotmail.com>
2024-06-29 17:35:29 -0400

    added advanced meta tags for Pages Router

--- src/pages/detailingServices.tsx
+++ src/pages/detailingServices.tsx
@@ -1,15 +1,15 @@
 // src/pages/services.js
 
-import React from "react";
-import ServicesPage from "../components/ui/ServicesPage";
-import Layout from "../components/ui/Layout";
+// import React from "react";
+// import ServicesPage from "../components/ui/ServicesPage";
+// import Layout from "../components/ui/Layout";
 
-const detailingServices = () => {
-  return (
-    <Layout>
-      <ServicesPage />;
-    </Layout>
-  );
-};
+// const detailingServices = () => {
+//   return (
+//     <Layout>
+//       <ServicesPage />;
+//     </Layout>
+//   );
+// };
 
-export default detailingServices;
+// export default detailingServices;
--- src/pages/interiorDetailingServices.tsx
+++ src/pages/interiorDetailingServices.tsx
@@ -19,6 +19,41 @@ const InteriorServicesPage: React.FC = () => {
           name="keywords"
           content="interior car detailing near me, car interior cleaning near me, interior detailing near me, Montreal car detailing, interior car wash"
         />
+        <meta name="robots" content="index, follow" />
+        <meta name="googlebot" content="index, follow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta charSet="utf-8" />
+        <meta property="og:site_name" content="Sam's Mobile Detailing" />
+        <meta property="og:locale" content="en_US" />
+        <meta
+          property="og:title"
+          content="Interior Car Detailing Services in Montreal | Sam's Mobile Detailing"
+        />
+        <meta
+          property="og:description"
+          content="Experience top-notch interior car detailing services in Montreal. Our expert team will leave your car's interior spotless and fresh. Book now!"
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content="https://www.samsmobiledetailing.ca/interiorDetailingServices"
+        />
+        <meta
+          property="og:image"
+          content="https://www.samsmobiledetailing.ca/images/other/weathermat.png"
+        />
+        <meta property="og:image:alt" content="Interior car detailing" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta
+          property="article:published_time"
+          content="2024-01-11T11:35:00+07:00"
+        />
+        <meta
+          property="article:modified_time"
+          content="2024-01-11T11:35:00+07:00"
+        />
       </Head>
       <InteriorServices />
     </Layout>
